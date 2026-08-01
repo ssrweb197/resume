98a7b20bd95aab62d796d7901b02131f45ebe1ee
BrandBlitz <ssraoh331@gmail.com>
2026-08-01 21:24:56 +0530

    Update resume-save.js

--- resume-save.js
+++ resume-save.js
@@ -1,1 +1,9 @@
+import { auth, db } from "./firebase-config.js";
+
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 console.log("Resume Save Module Loaded");
